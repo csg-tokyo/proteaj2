dfda41beadce9ef923b15096b946236ed1354481
Kazuhiro Ichikawa <ichikawa@csg.ci.i.u-tokyo.ac.jp>
2015-04-14 19:03:01 +0900

    unifyG のテストを追加

--- src/test/scala/phenan/prj/UnificationTest.scala
+++ src/test/scala/phenan/prj/UnificationTest.scala
@@ -38,6 +38,31 @@ class UnificationTest extends FunSuite with Matchers {
     val stringType = compiler.classLoader.loadClass("java/lang/String").get.objectType(Nil).get
     val ssMap = compiler.classLoader.loadClass("java/util/Map").get.objectType(List(stringType, stringType)).get
     ssMap.methods.get("entrySet") shouldBe a [Some[_]]
+    ssMap.methods.get("entrySet").get should have size 1
 
+    val entrySetMethod = ssMap.methods.get("entrySet").get.head
+
+    val ssMapEntryType = compiler.classLoader.loadClass("java/util/Map$Entry").get.objectType(List(stringType, stringType)).get
+    val setOfssMapEntryType = compiler.classLoader.loadClass("java/util/Set").get.objectType(List(ssMapEntryType)).get
+
+    val map = setOfssMapEntryType <=< entrySetMethod.returnType
+
+    map shouldBe Some(ssMap.env)
+  }
+
+  test ("Stream<String> s; List<String> list = s.map(...);") {
+    val stringType = compiler.classLoader.loadClass("java/lang/String").get.objectType(Nil).get
+    val sStream = compiler.classLoader.loadClass("java/util/stream/Stream").get.objectType(List(stringType)).get
+    sStream.methods.get("map") shouldBe a [Some[_]]
+    sStream.methods.get("map").get should have size 1
+
+    val mapMethod = sStream.methods.get("map").get.head
+
+    val stringListType = compiler.classLoader.loadClass("java/util/List").get.objectType(List(stringType)).get
+    val sListStream = compiler.classLoader.loadClass("java/util/stream/Stream").get.objectType(List(stringListType)).get
+
+    val map = sListStream <=< mapMethod.returnType
+
+    map shouldBe Some(sStream.env + ("R" -> stringListType))
   }
 }
