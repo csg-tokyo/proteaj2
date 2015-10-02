aaaf170e8c1dbfd979d0427839f9d23e276bd2e6
Kazuhiro Ichikawa <ichikawa@csg.ci.i.u-tokyo.ac.jp>
2015-10-02 18:07:08 +0900

    四則演算を実装

--- src/main/java/proteaj/lang/PredefOperators.java
+++ src/main/java/proteaj/lang/PredefOperators.java
@@ -21,4 +21,52 @@ public class PredefOperators {
   public static int addInt (int a, int b) {
     return a + b;
   }
+
+  @Operator(
+      priority = @Priority(dsl = "Lproteaj/lang/PredefOperators;", name = "add"),
+      pattern = {
+          @OpElem(kind = OpElemType.Hole, priority = @Priority(dsl = "Lproteaj/lang/PredefOperators;", name = "add")),
+          @OpElem(kind = OpElemType.Name, name = "-"),
+          @OpElem(kind = OpElemType.Hole)
+      }
+  )
+  public static int subInt (int a, int b) {
+    return a - b;
+  }
+
+  @Operator(
+      priority = @Priority(dsl = "Lproteaj/lang/PredefOperators;", name = "mul"),
+      pattern = {
+          @OpElem(kind = OpElemType.Hole, priority = @Priority(dsl = "Lproteaj/lang/PredefOperators;", name = "mul")),
+          @OpElem(kind = OpElemType.Name, name = "*"),
+          @OpElem(kind = OpElemType.Hole)
+      }
+  )
+  public static int mulInt (int a, int b) {
+    return a * b;
+  }
+
+  @Operator(
+      priority = @Priority(dsl = "Lproteaj/lang/PredefOperators;", name = "mul"),
+      pattern = {
+          @OpElem(kind = OpElemType.Hole, priority = @Priority(dsl = "Lproteaj/lang/PredefOperators;", name = "mul")),
+          @OpElem(kind = OpElemType.Name, name = "/"),
+          @OpElem(kind = OpElemType.Hole)
+      }
+  )
+  public static int divInt (int a, int b) {
+    return a / b;
+  }
+
+  @Operator(
+      priority = @Priority(dsl = "Lproteaj/lang/PredefOperators;", name = "mul"),
+      pattern = {
+          @OpElem(kind = OpElemType.Hole, priority = @Priority(dsl = "Lproteaj/lang/PredefOperators;", name = "mul")),
+          @OpElem(kind = OpElemType.Name, name = "%"),
+          @OpElem(kind = OpElemType.Hole)
+      }
+  )
+  public static int modInt (int a, int b) {
+    return a % b;
+  }
 }
